575beb8bf38c02eeff62a3738b244212c8e0cb1a
Champion113 <sbalbright19@gmail.com>
2020-10-15 21:37:55 -0400

    added test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,9 @@
+const { TestScheduler } = require('jest');
 const Employee = require('../lib/Employee');
+
+test('can get name via getName', () =>{
+    const testName = 'Paris';
+    const emp = new Employee (testName)
+    expect(emp.getName()).toBe(testName);
+})
+
