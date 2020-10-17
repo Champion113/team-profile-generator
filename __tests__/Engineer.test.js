948e78a413045ceebd604d2d183d0edc39829f5a
Champion113 <sbalbright19@gmail.com>
2020-10-17 07:32:07 -0400

    added eng test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,13 @@
 const Engineer = require('../lib/Engineer.js');
-test ('create github username')
+//create github username test
+test ('create github username', ()=> {
+const githubUser = "username";
+const employee = new Engineer('Paris', 1, 'test@test.com', githubUser);
+expect(emp.github).toBe(githubUser);
+});
+//get github username via getGithub test
+test ('can get github username', ()=> {
+    const githubUser = "username";
+    const employee = new Engineer('Paris', 1, 'test@test.com', githubUser);
+    expect(emp.getGithub()).toBe(githubUser);
+    });
