11fd0e70ff49cc2c06b70ad0df32940043b0844c
Champion113 <sbalbright19@gmail.com>
2020-10-17 12:32:32 -0400

    added enigeer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,13 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
 //create github username test
-test ('create github username', ()=> {
-const githubUser = "username";
-const employee = new Engineer('Paris', 1, 'test@test.com', githubUser);
-expect(emp.github).toBe(githubUser);
+test('create github username', () => {
+    const githubUser = "username";
+    const employee = new Engineer('Paris', 1, 'test@test.com', githubUser);
+    expect(emp.github).toBe(githubUser);
 });
 //get github username via getGithub test
-test ('can get github username', ()=> {
+test('can get github username via getGithub', () => {
     const githubUser = "username";
     const employee = new Engineer('Paris', 1, 'test@test.com', githubUser);
     expect(emp.getGithub()).toBe(githubUser);
-    });
+});
+//getRole return engineer
+test('can getRole return Engineer', () => {
+    const testRole = 'Engineer';
+    const emp = new Engineer('Paris', 1, 'test@test.com', 'username');
+    expect(emp.getRole()).toBe(testRole);
+});
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const Player = require('../lib/Intern');
+//create github username test
+test('create github username', () => {
+    const githubUser = "username";
+    const employee = new Engineer('Paris', 1, 'test@test.com', githubUser);
+    expect(emp.github).toBe(githubUser);
+});
+//get github username via getGithub test
+test('can get github username via getGithub', () => {
+    const githubUser = "username";
+    const employee = new Engineer('Paris', 1, 'test@test.com', githubUser);
+    expect(emp.getGithub()).toBe(githubUser);
+});
+//getRole return engineer
+test('can getRole return Engineer', () => {
+    const testRole = 'Engineer';
+    const emp = new Engineer('Paris', 1, 'test@test.com', 'username');
+    expect(emp.getRole()).toBe(testRole);
+});
