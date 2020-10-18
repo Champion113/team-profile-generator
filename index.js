83bd34092145dc565609399214baf7e36743dbbf
Champion113 <sbalbright19@gmail.com>
2020-10-18 11:41:36 -0400

    css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');;
-const Manager = require('../lib/Manager');
-const Engineer = require('../lib/Engineer');
-const Intern = require('../lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 //  inquirer dependency install
 const { prompt } = require('inquirer');
 const path = require('path')
