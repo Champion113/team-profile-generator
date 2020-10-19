4729f79985dc165d156db5b0134500053da8b5d4
Champion113 <sbalbright19@gmail.com>
2020-10-18 23:50:04 -0400

    fix offnum error

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,21 +3,21 @@ const Employee = require('../lib/Employee');
 
 //set name test
 test('can set name', () => {
-    const emp = 'Paris';
-    // const emp = new Employee(testName)
-    expect(emp.name).toEqual(expect.any(string));
+    const name = 'Paris';
+    const emp = new Employee(name)
+    expect(emp.name).toBe(name);
 })
 //set email
 test('can set email', () => {
-    // const testEmail = 'test@test.com';
-    const emp = new Employee('email', 1, "test@test.com");
-    expect(emp.email).toBe("test@test.com");
+    const testEmail = 'test@test.com';
+    const emp = new Employee('Ava', 1, testEmail);
+    expect(emp.email).toBe(testEmail);
 })
 //set id
 test('can set id', () => {
-    // const testId = '1';
-    const emp = new Employee('id', 1);
-    expect(emp.id).toBe(1);
+    const testId = '1';
+    const emp = new Employee('Ava', 1);
+    expect(emp.id).toBe(testId);
 })
 
 //get name test
@@ -29,13 +29,13 @@ test('can get name via getName', () => {
 //get Email test
 test('can get email via getEmail', () => {
     const testEmail = 'test@test.com';
-    const emp = new Employee('email', 1, testEmail);
+    const emp = new Employee('Ava', 1, testEmail);
     expect(emp.getEmail()).toBe(testEmail);
 })
 //get Id test
 test('can get id via getId', () => {
     const testId = '1';
-    const emp = new Employee('id', testId);
+    const emp = new Employee('Ava', testId);
     expect(emp.getId()).toBe(testId);
 })
 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,26 +1,18 @@
 const Manager = require('../lib/Manager');
-// const Employee = require('../lib/Employee')
+ const Employee = require('../lib/Employee')
 //create office number
 test('can set office number via constructor', () => {
-    const offNum = '100';
+    const offNum = 100;
     const emp = new Manager('Paris', 1, 'test@test.com', offNum);
     expect(emp.officeNumber).toBe(offNum);
-});
-//get officer number via getOfficeNumber test
-test('can get office number via getOfficeNumber', () => {
-    const officeNumber = '100';
-    const emp = new Manager('Paris', 1, 'test@test.com', officeNumber);
-    expect(emp.getOfficeNumber()).toBe(100);
-});
+    test("Can get office number via getOffice()", () => {
+        const testValue = 100;
+        const e = new Manager("Foo", 1, "test@test.com", testValue);
+        expect(e.getOfficeNumber()).toBe(testValue);
+      });
 //getRole return manager
 test('can getRole return manager', () => {
     const testRole = 'Manager';
     const emp = new Manager('Paris', 1, 'test@test.com', 100);
     expect(emp.getRole()).toBe(testRole);
-});
-//getRole return manager
-test('getRole return manager', () => {
-    const testMan = 'Manager';
-    const emp = new Manager('Paris', 1, 'test@test.com', 100);
-    expect(emp.getRole()).toBe(testMan);
-});
+});})
